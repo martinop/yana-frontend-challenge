5a77462c495f98d8cbae3a0a0438e2b908dca267
Martin Ocando <martinocandop@gmail.com>
2022-12-29 12:32:45 -0300

    min length for userName changed

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -6,7 +6,7 @@ const MIN_LENGHT = (min: number) =>
   `El campo debe tener al menos ${min} caracteres`;
 
 export const authSchema = Yup.object().shape({
-  userName: Yup.string().required(REQUIRED).min(6, MIN_LENGHT(6)),
+  userName: Yup.string().required(REQUIRED).min(3, MIN_LENGHT(3)),
   email: Yup.string().required(REQUIRED).email(INVALID),
   password: Yup.string().required(REQUIRED).min(6, MIN_LENGHT(6)),
 });
